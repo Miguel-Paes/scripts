4855c72e85c14cf82e8753b6ec47845157e6ae81
Miguel Paes da Silva <miguelpaesdasilva@gmail.com>
2025-06-08 18:39:33 -0300

    feat: adiciona comando para remover cache.

--- fix-pgp-manjaro.sh
+++ fix-pgp-manjaro.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script fixes the PGP key issues in Manjaro by updating the keyring and refreshing keys.
 
+sudo rm /var/lib/pacman/sync/* -rf                                                                        
 sudo pacman-key --init
 sudo pacman-key --populate archlinux manjaro
 sudo pacman -Syy
