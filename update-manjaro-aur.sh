190605a8fb7d0d950844ffe52620833e770af0f2
Miguel Paes da Silva <miguelpaesdasilva@gmail.com>
2025-09-04 14:45:46 -0300

    sudo pacman -Suy --noconfirm yay -Syu --noconfirm

--- update-manjaro-aur.sh
+++ update-manjaro-aur.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # This script updates Manjaro and AUR packages, and cleans up unnecessary files.
 
+echo "Updating Oficial Packages"
 sudo pacman -Suy --noconfirm
+echo -e "\n"
+echo "Updating AUR Packages"
 yay -Syu --noconfirm
