e46e7db7c82609d8368ecf930281279a4d6a04da
Miguel Paes da Silva <164786603+Miguel-Paes@users.noreply.github.com>
2026-03-23 14:19:45 -0300

    Enhance install-nvmnode.sh for NVM and Node.js
    Updated script to install the latest versions of NVM and Node.js.

--- install-nvmnode.sh
+++ install-nvmnode.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # This script install Nvm and Node latest versions. Can be used to update also.
 
-echo
+echo "Adding nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+echo "Adding nvm latest version"
+nvm install --lts
+
+echo "New versions"
+
+node -v
+npm -v
